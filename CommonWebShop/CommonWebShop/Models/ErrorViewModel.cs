5b3578f9b7bafc29613236075931566562aca54e
NaibOfDesert <tryc.artur@gmail.com>
2023-04-14 18:56:33 +0200

    Template

---
+++ CommonWebShop/CommonWebShop/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CommonWebShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
