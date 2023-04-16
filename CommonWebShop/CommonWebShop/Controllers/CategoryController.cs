2a0f988c7db71cb460c0f4e96350de7e57845d50
NaibOfDesert <tryc.artur@gmail.com>
2023-04-16 23:58:27 +0200

    DbContext & database category basic.

---
+++ CommonWebShop/CommonWebShop/Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using CommonWebShop.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommonWebShop.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var objCategoryList = _db.Categories.ToList();
+
+            
+            return View(objCategoryList);
+        }
+    }
+}
---
+++ CommonWebShop/CommonWebShop/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CommonWebShop.Models
+{
+    public class Category
+    {
+        [Key] // primaty key
+        public int Id { get; set; } // primary key can by set without [Key] by adding CategoryId    
+        [Required]
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
